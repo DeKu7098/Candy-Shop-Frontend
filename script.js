e6bc8345f703ba8136237c4e34e8fd592cc5c3e0
Arka Chakraborty <arkac720@gmail.com>
2023-06-29 23:19:54 +0530

    Third commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.addEventListener("DOMContentLoaded", form);
 let addButton = document.querySelector("#addButton");
-let candyname = document.querySelector("#candyName");
-let candydescription = document.querySelector("#candyDescription");
-let candyprice = document.querySelector("#candyPrice");
-let candyquantity = document.querySelector("#candyQuantity");
-let list = document.querySelector(".list");
+let candyName = document.querySelector("#candyName");
+let candyDescription = document.querySelector("#candyDescription");
+let candyPrice = document.querySelector("#candyPrice");
+let candyQuantity = document.querySelector("#candyQuantity");
+let list = document.querySelector("#list");
 
 addButton.addEventListener("click", form_submit);
 
@@ -12,12 +12,12 @@ async function form_submit(e) {
 	try {
 		e.preventDefault();
 		let obj = {
-			name: candyname.value,
-			description: candydescription.value,
-			price: candyprice.value,
-			quantity: candyquantity.value,
+			name: candyName.value,
+			description: candyDescription.value,
+			price: candyPrice.value,
+			quantity: candyQuantity.value,
 		};
-		candyname.value = candydescription.value = candyprice.value = candyquantity.value = "";
+		candyName.value = candyDescription.value = candyPrice.value = candyQuantity.value = "";
 		await toDatabase(obj);
 		form();
 	} catch (error) {
@@ -38,34 +38,34 @@ async function form() {
 	if (document.querySelector("ul")) {
 		document.querySelector("ul").remove();
 	}
-	let newlist = document.createElement("ul");
+	let newList = document.createElement("ul");
 
 	let res = await fromDatabase();
 
 	for (let i = 0; i < res.length; i++) {
-		let listitem = document.createElement("li");
-		listitem.setAttribute("id", res[i].id);
-		listitem.setAttribute("quant", res[i].quantity);
-		listitem.setAttribute("name", res[i].name);
+		let listItem = document.createElement("li");
+		listItem.setAttribute("id", res[i].id);
+		listItem.setAttribute("quant", res[i].quantity);
+		listItem.setAttribute("name", res[i].name);
 
 		let text = document.createTextNode(`${res[i].name}  ${res[i].description}  ${res[i].price}  ${res[i].quantity}`);
 
-		listitem.appendChild(text);
-		newlist.appendChild(listitem);
+		listItem.appendChild(text);
+		newList.appendChild(listItem);
 		let buy1 = document.createElement("button");
 		let buy2 = document.createElement("button");
 		let buy3 = document.createElement("button");
 		buy1.appendChild(document.createTextNode("buy 1"));
 		buy2.appendChild(document.createTextNode("buy 2"));
 		buy3.appendChild(document.createTextNode("buy 3"));
-		listitem.appendChild(buy1);
-		listitem.appendChild(buy2);
-		listitem.appendChild(buy3);
-		buy1.addEventListener("click", reduce1);
-		buy2.addEventListener("click", reduce2);
-		buy3.addEventListener("click", reduce3);
+		listItem.appendChild(buy1);
+		listItem.appendChild(buy2);
+		listItem.appendChild(buy3);
+		buy1.addEventListener("click", deductByOne);
+		buy2.addEventListener("click", deductByTwo);
+		buy3.addEventListener("click", deductByThree);
 	}
-	list.appendChild(newlist);
+	list.appendChild(newList);
 }
 
 
@@ -80,66 +80,66 @@ async function fromDatabase() {
 }
 
 
-function reduce1(e) {
+function deductByOne(e) {
 	let id = e.target.parentNode.getAttribute("id");
 	let quant = e.target.parentNode.getAttribute("quant");
-	let candyname = e.target.parentNode.getAttribute("name");
+	let candyName = e.target.parentNode.getAttribute("name");
 	quant = quant - 1;
 	if (quant < 0) {
-		alert(`you cannot buy more ${candyname}`);
+		alert(`you cannot buy more ${candyName}`);
 		e.target.parentNode.remove();
-		deleteDatabase(id);
+		deleteCandy(id);
 	} else {
-		editDatabase(id, quant);
+		updateCandy(id, quant);
 	}
 }
 
-function reduce2(e) {
+function deductByTwo(e) {
 	let id = e.target.parentNode.getAttribute("id");
 	let quant = e.target.parentNode.getAttribute("quant");
-	let candyname = e.target.parentNode.getAttribute("name");
+	let candyName = e.target.parentNode.getAttribute("name");
 
 	quant = quant - 2;
 
 	if (quant < 0) {
-		alert(`you cannot buy more ${candyname}`);
+		alert(`you cannot buy more ${candyName}`);
 		e.target.parentNode.remove();
-		deleteDatabase(id);
+		deleteCandy(id);
 	} else {
-		editDatabase(id, quant);
+		updateCandy(id, quant);
 	}
 }
 
-function reduce3(e) {
+function deductByThree(e) {
 	let id = e.target.parentNode.getAttribute("id");
 	let quant = e.target.parentNode.getAttribute("quant");
-	let candyname = e.target.parentNode.getAttribute("name");
+	let candyName = e.target.parentNode.getAttribute("name");
 
 	quant = quant - 3;
 
 	if (quant < 0) {
-		alert(`you cannot buy more ${candyname}`);
+		alert(`you cannot buy more ${candyName}`);
 		e.target.parentNode.remove();
-		deleteDatabase(id);
+		deleteCandy(id);
 	} else {
-		editDatabase(id, quant);
+		updateCandy(id, quant);
 	}
 }
 
-async function editDatabase(id, quant) {
+async function updateCandy(id, quant) {
 	try {
 		let obj = {
 			quantity: quant,
 		};
 		await axios.patch(`http://localhost:3000/userr/candy/${id}`, obj);
-		console.log("obj patched");
+		console.log("candy updated");
 		form();
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-async function deleteDatabase(id) {
+async function deleteCandy(id) {
 	try {
 		await axios.delete(`http://localhost:3000/userr/candy/${id}`);
 		console.log("deleted");
